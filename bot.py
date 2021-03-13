203a4e37b996fe3e1c4c8d34ebb577b9c1bf119c
rasulovmuxtor <rasulovoff@gmail.com>
2021-03-14 02:08:33 +0500

    added files

---
+++ bot.py
@@ -0,0 +1,78 @@
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import sqlite3
+
+from export import excel,pdf
+
+def is_new(user_id):
+    with sqlite3.connect('Murojaat.sqlite3') as conn:
+        conn.row_factory = lambda cursor, row: row[0]
+        cursor = conn.cursor()
+        try:
+            murojaat = cursor.execute("SELECT * FROM murojaatlar where user_id=?",(user_id,)).fetchall()
+            if murojaat == []:
+                return True
+            return murojaat
+        except Exception as e:
+            print(e)
+
+yangi_murojaat_inline = InlineKeyboardMarkup()
+yangi_murojaat_inline.add(InlineKeyboardButton("Yangi Murojaat yaratish",callback_data='new'))
+
+TOKEN = "1663406803:AAHRMKNA65Tn9iqzvQ6hL8EiOgI3lDvj_OA"
+
+bot = TeleBot(token=TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    step_message = bot.send_message(msg.from_user.id,"Assalomu alaykum! \n\nIsm, familyangizni kiriting")
+    bot.register_next_step_handler(step_message,anketa_name)
+
+
+def anketa_name(msg):
+    name = msg.text
+    with sqlite3.connect('Murojaat.sqlite3') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("INSERT INTO murojaatlar (user_id,name) values(?,?)",(msg.from_user.id,name))
+        except Exception as e:
+            print(e)
+            step_message = bot.reply_to(msg,"Xatolik yuz berdi, Iltimos ism, familyangizni qayta kiriting!")
+            bot.register_next_step_handler(step_message, anketa_name)
+            return
+    step_message = bot.send_message(msg.from_user.id,"Murojaatingizni matn ko'rinishida yozib qoldiring\n\nEslatma: Murojaat faqat bitta habardan iborat bo'ladi!")
+    bot.register_next_step_handler(step_message, murojaat_text)
+
+def murojaat_text(msg):
+    content = msg.text
+    with sqlite3.connect('Murojaat.sqlite3') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("Update murojaatlar set murojaat = ? where id=(SELECT max(id) FROM murojaatlar where user_id=?)", (content,msg.from_user.id))
+        except Exception as e:
+            print(e)
+            step_message = bot.reply_to(msg, "Xatolik yuz berdi, Iltimos Murojaatnoma habarini qayta yuboring!")
+            bot.register_next_step_handler(step_message, murojaat_text)
+    bot.send_message(msg.from_user.id,'Murojaatingiz qabul qilindi',reply_markup=yangi_murojaat_inline)
+
+@bot.callback_query_handler(func=lambda call:call.data=='new')
+def set_lang1(call):
+    bot.delete_message(call.from_user.id, call.message.message_id)
+    step_message = bot.send_message(call.from_user.id, "Ism, familyangizni kiriting!")
+    bot.register_next_step_handler(step_message, anketa_name)
+
+
+@bot.message_handler(commands=['excel'])
+def export_excel(msg):
+    bot.send_document(msg.from_user.id,excel())
+
+@bot.message_handler(commands=['pdf'])
+def export_excel(msg):
+    bot.send_document(msg.from_user.id,pdf())
+
+@bot.message_handler(func=lambda x:True)
+def yangi_murojaat_uchun(msg):
+    bot.send_message(msg.from_user.id, "Yangi murojaatnoma yozish uchun pastdagi tugmani bo'sing", reply_markup=yangi_murojaat_inline)
+
+if __name__ == '__main__':
+    bot.polling()
---
+++ export.py
@@ -0,0 +1,80 @@
+import sqlite3
+from xlsxwriter.workbook import Workbook
+
+
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import LongTable, BaseDocTemplate,Frame, PageTemplate
+from reportlab.lib import colors
+from reportlab.platypus import Paragraph, TableStyle
+
+
+def excel():
+    workbook = Workbook("Murojaatnomalar.xlsx")
+    worksheet = workbook.add_worksheet()
+    worksheet.set_default_row(30)
+    worksheet.set_column(1,1,200.1)
+    worksheet.set_column(0,0,20.1)
+    worksheet.write(0, 0, "Ism-Familya")
+    worksheet.write(0, 1, "Murojaatnoma")
+
+    cell_format = workbook.add_format()
+    cell_format.set_text_wrap()
+
+    with sqlite3.connect('Murojaat.sqlite3') as conn:
+        c = conn.cursor()
+        mysel = c.execute("select name,murojaat from murojaatlar")
+    for i, row in enumerate(mysel, 1):
+        worksheet.write(i, 0, row[0])
+        worksheet.write(i, 1, row[1],cell_format)
+    workbook.close()
+    return open("Murojaatnomalar.xlsx",'rb')
+
+def pdf():
+    doc = BaseDocTemplate(
+        "Murojaatnomalar.pdf",
+        pagesize=A4,
+        rightMargin=3,
+        leftMargin=1,
+        topMargin=0,
+        bottomMargin=4,
+        showBoundary=False)
+
+    elements = []
+
+    with sqlite3.connect('Murojaat.sqlite3') as conn:
+        conn.row_factory = lambda cursor, row: [row[0],row[1]]
+        c = conn.cursor()
+        data = c.execute("select name,murojaat from murojaatlar").fetchall()
+    data = [["Ism-Familya","Murojaatnoma"]]+data
+
+    tableStyle = [
+        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+        ('ALIGN', (0, 0), (0, -1), "LEFT"),
+        ('FONTSIZE', (0, 0), (-1, -1), 14),
+        ('ALIGN', (0, 7), (0, 7), "RIGHT"),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP')
+        ]
+
+
+    styles = getSampleStyleSheet()
+    styleN = styles['Normal']
+
+    styleN.wordWrap = 'CJK'
+
+    data2 = [[Paragraph(cell, styleN) for cell in row] for row in data]
+
+    frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height - 2 * cm, id='normal')
+
+    colwidths = [frame._width/5.,4*frame._width/5.]
+
+    t = LongTable(data2, colWidths=colwidths)
+    t.setStyle(TableStyle(tableStyle))
+    elements.append(t)
+
+    template = PageTemplate(id='longtable', frames=frame)
+    doc.addPageTemplates([template])
+    doc.build(elements)
+    return open("Murojaatnomalar.pdf",'rb')
